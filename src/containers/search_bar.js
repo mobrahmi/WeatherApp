6a8d31c68b7639aa525f780ce909bdc8fb331cc8
mouhe4web <mohammed26brahmi@gmail.com>
2017-09-06 09:16:40 +0100

    first commit

---
+++ src/containers/search_bar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class searchBar extends Component {
+     render(){
+         return(
+             <form className='input-group'>
+                 <input />
+             </form>
+         );
+     }
+
+}
